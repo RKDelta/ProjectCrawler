5554109eb7c5b52aeb92b1179b8dffe78fc9020a
R3dNW <R3dNW@users.noreply.github.com>
2016-11-20 19:11:56 +0000

    Metal Blades (#10)
    Added Some Metal Blades to the game Files for use at some point.

---
+++ ProjectCrawler/Assets/WorldController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class WorldController : MonoBehaviour
+{
+    public int width = 32;
+    public int height = 32;
+
+    public GameObject[,] tiles;
+
+    Dictionary<string, Sprite> groundSprites;
+
+    public void Start()
+    {
+        tiles = new GameObject[width, height];
+
+        Sprite[] sprites = Resources.LoadAll<Sprite>("mudland_floor");
+
+        groundSprites = new Dictionary<string, Sprite>();
+
+        foreach (Sprite sprite in sprites)
+        { 
+            groundSprites.Add(sprite.name, sprite);
+        }
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                tiles[x, y] = new GameObject(
+                    string.Format("tile_{0}_{1}", x, y),
+                    typeof(SpriteRenderer));
+
+                tiles[x, y].GetComponent<SpriteRenderer>().sprite =
+                    groundSprites["mudland_floor_" + ((8 * (7 - (y % 8))) + (x % 8)).ToString()];
+
+                tiles[x, y].transform.position = new Vector2(x, y);
+            }
+        }
+    }
+}
